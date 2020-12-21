2bf06cddb0782d4ad790b3c7be12e77853d22c09
Sergio <srosduran@gmail.com>
2020-12-21 10:45:53 +0100

    uninstalled plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,20 +5,11 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-offline`,
-    `gatsby-plugin-og-images`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images/`,
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-og-images',
-      options: {
-        template: 'og',
-        domain: process.env.GATSBY_DOMAIN,
-        debug: true,
       },
     },
     {
